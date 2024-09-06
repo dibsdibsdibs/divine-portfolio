f47d7a0282880e94b36e85786f84ff1ff2164b0e
Divine <divinelavente@gmail.com>
2024-09-06 22:11:48 +0800

    animated buttons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,6 @@
-import { FaEnvelope, FaLinkedin, FaGithub} from "react-icons/fa6"; 
+"use client";
+import { FaEnvelope, FaLinkedin, FaGithub} from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 function Footer() {
   const about={
@@ -11,15 +13,30 @@ function Footer() {
       <div className="mt-auto w-full flex items-center justify-between p-5 bg-violet text-white font-raleway font-bold">
         <p>© Divine Grace Lavente</p>
         <div className="flex w-20 justify-between">
-          <a href={`${"mailto:"} ${about.email}`}>
-            <FaEnvelope />
-          </a>
-          <a href={about.linkedin}>
-            <FaLinkedin />
-          </a>
-          <a href={about.github}>
-            <FaGithub />
-          </a>
+          <motion.button
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href={`${"mailto:"} ${about.email}`}>
+              <FaEnvelope />
+            </a>
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href={about.linkedin}>
+              <FaLinkedin />
+            </a>
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href={about.github}>
+              <FaGithub />
+            </a>
+          </motion.button>
         </div>
       </div>
     );
